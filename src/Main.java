8c2e93bb0858cb363cccd2a7eff8ea7d212966ee
HJL <HJL.com>
2023-06-08 17:20:34 +0800

    push test commit

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("hot-fix first commit");
         System.out.println("hot-fix conflict");
         System.out.println("mast_conflict");
+        System.out.println("push test");
     }
 }
